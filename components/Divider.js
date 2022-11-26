4b9011d74e49f4f76abdb5a2a8e08804e881105f
Pat <ppevans11@gmail.com>
2022-11-26 15:35:19 +1300

    :sparkles: Update `Divider` component

--- components/Divider.js
+++ components/Divider.js
@@ -6,13 +6,17 @@
 const dividerStyle = {
     padding: 5,
     margin: '0 auto',
-    width: '66%'
+    width: '50%'
+}
+
+const colorStyle = {
+    color: '#4CAF50'
 }
 
 // Divider component
 const Divider = () => (
     <div style={dividerStyle}>
-        <hr />
+        <hr style={colorStyle} />
     </div>
 )
 
